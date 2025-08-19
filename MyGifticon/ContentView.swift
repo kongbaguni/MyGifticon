088e2f8feda0e077a7d2b200199a94394d8af3ae
서창열 <kongbaguni@gmail.com>
2025-08-19 20:35:30 +0900

    Initial Commit

---
+++ MyGifticon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyGifticon
+//
+//  Created by Changyeol Seo on 8/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyGifticon/MyGifticonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGifticonApp.swift
+//  MyGifticon
+//
+//  Created by Changyeol Seo on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MyGifticonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
