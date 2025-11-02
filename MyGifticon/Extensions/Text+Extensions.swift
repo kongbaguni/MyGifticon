285d8c59e1f97f71611586df5c583ea9623dd090
서창열 <kongbaguni@gmail.com>
2025-11-02 21:52:28 +0900

    주석 추가

--- MyGifticon/Extensions/Text+Extensions.swift
+++ MyGifticon/Extensions/Text+Extensions.swift
@@ -7,11 +7,12 @@
 
 import SwiftUI
 extension Text {
+    /** 버전 */
     static var versionName:Text {
         let text = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
         return .init(text)
     }
-    
+    /** 빌드번호 */
     static var buildNumber:Text {
         .init(Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown")
     }
