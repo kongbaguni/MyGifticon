710e4f77b6b906e744b58830f9188e129350ff1f
서창열 <kongbaguni@gmail.com>
2025-11-02 20:45:18 +0900

    cloudkit 추가

--- MyGifticon/MyGifticonApp.swift
+++ MyGifticon/MyGifticonApp.swift
@@ -23,11 +23,23 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct MyGifticonApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            GifticonModel.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: [GifticonModel.self])
+        .modelContainer(sharedModelContainer)
         
     }
 }
